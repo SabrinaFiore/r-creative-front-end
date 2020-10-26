eb12c169d91cdde72f3d9ed64321f3b7d0d215e5
Sabrina Fiore <sabrinafiore@Sabrinas-MBP.home-life.hub>
2020-10-26 12:06:03 +0100

    hero: add hero-section to three.js layout

--- src/app/views/r-creative-three/r-creative-three.component.ts
+++ src/app/views/r-creative-three/r-creative-three.component.ts
@@ -10,6 +10,9 @@ export class RCreativeThreeComponent {
   
   @ViewChild('rendererContainer') rendererContainer: ElementRef;
 
+  title: string = 'Who we are';
+  description: string = 'New concepts, methods and technologies are completely revolutionizing  traditional methods of project planning, design and construction. Implementing these changes to achieve the promise of higher quality standards, shorter turnaround times and lower costs requires totally rethinking Design and Construction Management today RIMOND is an exceptionally capable partner in this area.  We bring innovative tools and precision to the design, construction and acquisition of buildings, particularly in complex projects where attention to each individual aspect of the process is critical to achieving the brilliant results our clients expect.In this rapidly evolving context, we are confident in our ability to satisfy architects ambitions, developers needs for sustainability and clients profitability by ensuring excellence throughout each project.At RIMOND, we are breaking ground for tomorrow.';
+
   renderer = new THREE.WebGLRenderer();
   scene = null;
   camera = null;
@@ -20,9 +23,10 @@ export class RCreativeThreeComponent {
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
     this.camera.position.z = 1000;
 
-    const geometry = new THREE.BoxGeometry(200, 200, 200);
+    const geometry = new THREE.BoxGeometry(100, 100, 100);
     const material = new THREE.MeshNormalMaterial();
     this.mesh = new THREE.Mesh(geometry, material);
+    this.mesh.position.set(500, 100, 50);
 
     this.scene.add(this.mesh);
   }
